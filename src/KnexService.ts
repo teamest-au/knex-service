e816db7cb3cfe60f15a745a20e6d04de1f32f973
Daniel Emery <danielremery@gmail.com>
2020-11-07 17:19:18 +1030

    Updated to support new process manager

--- src/KnexService.ts
+++ src/KnexService.ts
@@ -4,6 +4,7 @@ import {
   ILogger,
   IProcessManagerService,
   IServiceHealth,
+  IServiceStatus,
 } from '@teamest/process-manager';
 
 export interface IKnexServiceOptions {
@@ -17,10 +18,12 @@ export default class KnexService implements IProcessManagerService {
   logger: ILogger;
   options: IKnexServiceOptions;
   knex?: Knex;
+  state: 'stopped' | 'starting' | 'running' | 'stopping';
 
   constructor(logger: ILogger, options: IKnexServiceOptions) {
     this.logger = logger;
     this.options = options;
+    this.state = 'stopped';
   }
 
   getInstance(): Knex {
@@ -39,22 +42,29 @@ export default class KnexService implements IProcessManagerService {
       try {
         await this.knex.raw('select 1+1 as result');
         return {
-          healthy: true,
+          healthy: 'healthy',
         };
       } catch (err) {
         return {
-          healthy: false,
+          healthy: 'unhealthy',
           message: `Failed to connect to mysql: ${err}`,
         };
       }
     }
     return {
-      healthy: false,
+      healthy: 'unhealthy',
       message: 'Knex instance not initialised',
     };
   }
 
+  getStatus(): IServiceStatus {
+    return {
+      state: this.state,
+    };
+  }
+
   async start(): Promise<void> {
+    this.state = 'starting';
     this.logger.info(
       `Connecting to MySql ${this.options.user}@${this.options.host}`,
     );
@@ -65,9 +75,32 @@ export default class KnexService implements IProcessManagerService {
         tableName: 'migrations',
       },
     });
+    try {
+      await this.knex.raw('select 1+1 as result');
+    } finally {
+      this.state = 'running';
+    }
   }
 
   stop(): Promise<void> {
-    throw new Error('Method not implemented.');
+    this.logger.info('Attempting to close mysql connections')
+    this.state = 'stopping';
+    if (this.knex) {
+      return new Promise((res, rej) => {
+        try {
+          this.knex?.destroy(() => {
+            this.logger.info('mysql connections closed successfully');
+            this.state = 'stopped';
+            res();
+          });
+        } catch (err) {
+          rej(err);
+        }
+      });
+    } else {
+      this.state = 'stopped';
+      this.logger.info('No connections to close, stopped');
+      return Promise.resolve();
+    }
   }
 }
